0d43c59e1d2731112290071d3ece4cddb84f8dcb
Luc Terracher <lucterracher@gmail.com>
2016-12-11 21:42:19 +0100

    Stack fixed, webpack 2 ready

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,63 +1,70 @@
 var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var helpers = require('./helpers');
 
-module.exports = {
-  entry: {
-    'polyfills': './src/polyfills.ts',
-    'vendor': './src/vendor.ts',
-    'app': './src/main.ts'
-  },
+/*
+ * Webpack Plugins
+ */
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-  resolve: {
-    extensions: ['', '.ts', '.js', '.less', '.scss', '.css', '.html']
-  },
+/*
+ * Webpack Constants
+ */
+const HMR = helpers.hasProcessFlag('hot');
+const METADATA = {
+  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  baseUrl: '/',
+  isDevServer: helpers.isWebpackDevServer()
+};
 
-  module: {
-    loaders: [
-      {
-        test: /\.ts$/,
-        loaders: ['awesome-typescript-loader', 'angular2-template-loader']
-      },
-      {
-        test: /\.html$/,
-        loader: 'html'
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'file?name=assets/[name].[hash].[ext]'
-      },
-      {
-        test: /\.css$/,
-        exclude: helpers.root('src', 'app'),
-        loader: ExtractTextPlugin.extract('style', 'css?sourceMap')
-      },
-      {
-        test: /\.css$/,
-        include: helpers.root('src', 'app'),
-        loader: 'raw'
-      },
-      {
-        test: /\.less$/,
-        exclude: /node_modules/,
-        loader: 'raw!less'
-      },
-      {
-        test: /\.scss$/,
-        exclude: /node_modules/,
-        loaders: ['raw-loader', 'sass-loader']
-      }
+
+/*
+ * Webpack configuration
+ *
+ * See: http://webpack.github.io/docs/configuration.html#cli
+ */
+module.exports = function (options) {
+  return {
+    entry: {
+      'polyfills': './src/polyfills.ts',
+      'vendor': './src/vendor.ts',
+      'main': './src/main.ts'
+    },
+    resolve: {
+      /*
+       * An array of extensions that should be used to resolve modules.
+       *
+       * See: http://webpack.github.io/docs/configuration.html#resolve-extensions
+       */
+      extensions: ['.ts', '.js', '.json'],
+      // An array of directory names to be resolved to the current directory
+      modules: [helpers.root('src'), helpers.root('node_modules')],
+    },
+    module: {
+      rules: [
+        { test: /\.ts$/, exclude: /node_modules/, loader: ['awesome-typescript-loader', 'angular2-template-loader'] },
+        { test: /\.json$/, loader: 'json-loader' },
+        { test: /\.html/, loader: 'html-loader?minimize=false', exclude: [helpers.root('src/index.html')] },
+        { test: /\.scss$/, loader: 'raw-loader!sass-loader' },
+        { test: /\.css$/, loader: 'style-loader!css-loader' },
+        { test: /\.(gif|png|jpe?g)$/i, loader: 'file-loader?name=dist/images/[name].[ext]' },
+        { test: /\.woff2?$/, loader: 'url-loader?name=dist/fonts/[name].[ext]&limit=10000&mimetype=application/font-woff' },
+        { test: /\.(ttf|eot|svg)$/, loader: 'file-loader?name=dist/fonts/[name].[ext]' }
+      ]
+    },
+    plugins: [
+      new webpack.ContextReplacementPlugin(
+        /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+        __dirname
+      ),
+      
+      new webpack.optimize.CommonsChunkPlugin({
+        name: ['app', 'vendor', 'polyfills']
+      }),
+
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      })
     ]
-  },
-
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: ['app', 'vendor', 'polyfills']
-    }),
-
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
-  ]
+  }
 };
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,30 +1,54 @@
-var webpackMerge = require('webpack-merge');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var DashboardPlugin = require('webpack-dashboard/plugin');
-var commonConfig = require('./webpack.common.js');
 var helpers = require('./helpers');
+var path = require('path');
+var webpackMerge = require('webpack-merge');
+var commonConfig = require('./webpack.common.js');
 
-module.exports = webpackMerge(commonConfig, {
-  proxy: {
-    "/api/*" : "http://localhost:3000" // <- backend
-  },
+/**
+ * Webpack Plugins
+ */
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-  devtool: 'cheap-module-eval-source-map',
-
-  output: {
-    path: helpers.root('dist'),
-    publicPath: 'http://localhost:8080/',
-    filename: '[name].js',
-    chunkFilename: '[id].chunk.js'
-  },
-
-  plugins: [
-    new ExtractTextPlugin('[name].css'),
-    new DashboardPlugin()
-  ],
-
-  devServer: {
-    historyApiFallback: true,
-    stats: 'minimal',
-  }
+/**
+ * Webpack Constants
+ */
+const ENV = process.env.ENV = process.env.NODE_ENV = 'development';
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || 8080;
+const HMR = helpers.hasProcessFlag('hot');
+const METADATA = webpackMerge(commonConfig({env: ENV}).metadata, {
+  host: HOST,
+  port: PORT,
+  ENV: ENV,
+  HMR: HMR
 });
+
+
+
+
+module.exports = function(opt) {
+  return webpackMerge(commonConfig({env: ENV}), {
+    devtool: 'cheap-module-eval-source-map',
+
+    output: {
+      path: helpers.root('dist'),
+      publicPath: 'http://localhost:8080/',
+      filename: '[name].bundle.js',
+      chunkFilename: '[id].chunk.js'
+    },
+
+    plugins: [
+      new ExtractTextPlugin('[name].css')
+    ],
+
+    devServer: {
+      port: 8080,
+      historyApiFallback: true,
+      stats: 'minimal',
+      contentBase: path.join(__dirname, "src"),
+      watchOptions: {
+        aggregateTimeout: 300,
+        poll: 1000
+      }
+    }
+  });
+};
